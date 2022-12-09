78429ff183ce1f459e94462d6752aa72bd59de7f
Ildar Muhaev <ildar.muckhaev@gmail.com>
2022-12-09 03:19:03 +0300

    comments

--- src/main/java/coder/ShannonCoder.java
+++ src/main/java/coder/ShannonCoder.java
@@ -7,25 +7,33 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class ShannonCoder {
+	// словарь кодовых слов, (слово - код)
 	private static Map<String, String> codes;
 
 	public static void main(String[] args) {
 		try {
+			// считываем файл с алфавитом и вероятностями
+			File file = new File("src/main/java/file/shannon.txt");
+			FileReader fr = new FileReader(file);
+			BufferedReader reader = new BufferedReader(fr);
+
+			// вводим строку, которую хотим закодировать
 			Scanner sc = new Scanner(System.in);
 			String line = sc.nextLine();
 
-			File file = new File("src/main/java/file/shannon.txt");
-
-			FileReader fr = new FileReader(file);
-
-			BufferedReader reader = new BufferedReader(fr);
-
+			// считываем алфавит из файла
 			String alphabet = reader.readLine();
+
+			// считываем распределение вероятностей из файла
 			String probability = reader.readLine();
 
+			// получаем словарь из кодовых слов
 			codes = runShannonCode(alphabet, probability);
+
+			// выводим словарь (опционально)
 			printCodes();
 
+			// кодируем сообщение и выводим
 			System.out.println(toShannonCode(line));
 
 		} catch (IOException e) {
@@ -35,13 +43,20 @@ public class ShannonCoder {
 
 	private static String toShannonCode(String message) {
 		StringBuilder code = new StringBuilder("");
+
+		// кодируем посимвольно сообщение
 		for (int i = 0; i < message.length(); i++) {
+
+			// находим соответствующий символу код в словаре
 			String charCode = codes.get(String.valueOf(message.charAt(i)));
+
+			// добавляем найденный символ к конечному закодированному сообщению
 			code.append(charCode);
 		}
 		return code.toString();
 	}
 
+	// -- вспомогательный метод, выводим словарь кодов --
 	private static void printCodes() {
 		for (Map.Entry<String, String> entry : codes.entrySet()) {
 			String key = entry.getKey();
@@ -50,62 +65,101 @@ public class ShannonCoder {
 		}
 	}
 
+	// -- метод для кодировки словаря с помощью алгоритма Шеннона --
 	private static Map<String, String> runShannonCode(String alphabet, String probability) {
+
+		// переводим строку с алфавитом в массив
 		String[] alphabetArray = alphabet.split(" ");
+
+		// переводим строку с вероятностями в массив
 		String[] probabilityArray = probability.split(" ");
 
 		int length = alphabetArray.length;
+
+		// словарь с кодами
 		TreeMap<String, String> codes = new TreeMap<>();
 
+		// словарь символ алфавита - вероятность
 		HashMap<String, Double> map = new HashMap<>();
 		for (int i = 0; i < length; i++) {
 			map.put(alphabetArray[i], Double.parseDouble(probabilityArray[i]));
 		}
 
+
+		// сортируем данный словарь по убыванию вероятностей
 		Map<String, Double> sortedMap = map.entrySet().stream()
 				.sorted(Map.Entry.<String, Double>comparingByValue().reversed())
 				.collect(Collectors.toMap(
 						Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, TreeMap::new));
 
+		// переменная q, равная сумме предыдущих вероятностей, BigDecimal для точного сложения
 		BigDecimal[] q = new BigDecimal[length];
 
+		// вытаскиваем из словаря набор вероятностей и переводим его в массив, для удобства
 		Object[] p = sortedMap.values().toArray();
 
+		// вероятность предыдущего символа
 		double pPrev;
+
+		// вероятность текущего символа
 		double pCurrent;
 
+		// массив с логарифмами вероятностей
 		int[] l = new int[length];
 
 		for (int i = 0; i < length; i++) {
 			pCurrent = (double) p[i];
+
+			// q для нулевого элемента будет 0
 			if (i == 0) {
 				q[i] = BigDecimal.valueOf(0.0);
 			} else {
 				pPrev = (double) p[i - 1];
+
+				// находим q для текущего символа алфавита, q предыдущего + вероятность предыдущего
 				q[i] = BigDecimal.valueOf(pPrev).add(q[i - 1]);
 			}
+
+			// вычисляем логарифм для текущей вероятности
 			l[i] = -log2(pCurrent);
 		}
 
 		StringBuilder code = new StringBuilder("");
+
 		for (int i = 0; i < length; i++) {
+			// перебираем по порядку символы алфавита
 			String key = (String) sortedMap.keySet().toArray()[i];
+			// если это первый символ, его код будет повторением 0 l[0] раз
 			if (i == 0) {
 				code.append("0".repeat(l[0]));
+				// записываем код в словарь
 				codes.put(key, code.toString());
 			} else {
+				// переводим q из String в double
 				double qDouble = Double.parseDouble(q[i].toString());
+
+				System.out.println(qDouble);
+
+				// переводим дробную часть q из десятичной в двоичную систему счисления
 				String qCode = fractionalToBinary(qDouble, l[i]);
+
+				System.out.println(qCode);
+
+				// записываем код в словарь
 				codes.put(key, qCode);
 			}
 		}
+		// возвращаем конечный словарь
 		return codes;
 	}
 
+
+	// -- метод для нахождения log по основанию 2 --
 	public static int log2(double x) {
 		return (int) Math.floor(Math.log(x) / Math.log(2));
 	}
 
+	// -- метод для перевода дробной части в двоичную систему с точностью precision символов после запятой --
 	private static String fractionalToBinary(double num, int precision) {
 		StringBuilder binary = new StringBuilder();
 		while (binary.length() < precision) {
--- src/main/java/decoder/Decoder.java
+++ src/main/java/decoder/Decoder.java
@@ -11,19 +11,21 @@ public class Decoder {
 	public static void main(String[] args) {
 		try {
 			Scanner sc = new Scanner(System.in);
+
+			// считываем файл с алфавитом
+			File file = new File("src/main/java/file/stopka.txt");
+			FileReader fr = new FileReader(file);
+			BufferedReader reader = new BufferedReader(fr);
+			String alphabet = reader.readLine();
+
+			// вводим сообщение, номер столбца в Б-У
 			String message = sc.nextLine();
 			int indexBWT = sc.nextInt();
 
-			File file = new File("src/main/java/file/stopka.txt");
-
-			FileReader fr = new FileReader(file);
-
-			BufferedReader reader = new BufferedReader(fr);
-
-			String alphabet = reader.readLine();
+			// декодируем сообщение стопкой книг
 			String decodedMTF = decodeMTF(alphabet, message);
 
-			System.out.println(decodedMTF);
+			// декодируем сообщение Б-У
 			System.out.println(decodeBWT(decodedMTF, indexBWT));
 
 		} catch (
@@ -32,19 +34,23 @@ public class Decoder {
 		}
 	}
 
+	// -- декодер стопки книг --
 	private static String decodeMTF(String alphabet, String message) {
+
 		List<String> alphabetConstList = new ArrayList<>();
 
+		// превращаем строку с алфавитом в ArrayList, сортируем
 		alphabetConstList = Stream.of(alphabet.split(" "))
 				.sorted()
 				.map(String::new).toList();
 
-		List<String> unmodifiableList = Collections.unmodifiableList(alphabetConstList);
 
+		// превращаем ArrayList из неизменяемого в изменяемый, иначе операция add не работает
+		List<String> unmodifiableList = Collections.unmodifiableList(alphabetConstList);
 		List<String> alphabetList = new ArrayList<>(unmodifiableList);
 
+		// ArrayList с кодами, раздаем значения согласно индексам букв в алфавите
 		List<String> code = new ArrayList<>();
-
 		for (int i = 0; i < alphabetList.size(); i++) {
 			code.add(String.valueOf(i));
 		}
@@ -54,13 +60,21 @@ public class Decoder {
 		String letters = "";
 
 		for (char letter : message.toCharArray()) {
+			// текущий закодированный символ
 			letters = letters + letter;
 
+			// содержится ли в списке соответствующий сообщению код
 			if (code.contains(letters)) {
+				// индекс кода в листе кодов
 				int ind = code.indexOf(letters);
+
+				// достаем текущий символ по этому индексу
 				String let = alphabetList.get(ind);
+
+				// записываем его в конечное сообщение
 				decodedMessage.append(let);
 
+				// перемещаем элемент на верх стопки
 				alphabetList.remove(ind);
 				alphabetList.add(0, let);
 
@@ -71,35 +85,60 @@ public class Decoder {
 		return decodedMessage.toString();
 	}
 
+	// -- декодер Б-У -- (первый способ)
 	private static String decodeBWT(String message, int index) {
 		StringBuilder decodedMessage = new StringBuilder();
 
+		// количество повторений букву в верхней строчке
 		int repeat;
 
+		// запоминаем текущий индекс в верхней строке, чтобы идти по элементам до него
+		int position;
+
+		// конвертируем строку с сообщением в ArrayList (это будет наша нижняя строка)
 		List<String> messageList = new ArrayList<>(Arrays.asList(message.split("")));
 
+		// собираем отдельный лист с алфавитом и сортируем его, получаем верхнюю строку
 		List<String> alphabetList = new ArrayList<>(messageList);
-
-		List<Integer> indexes = new ArrayList<>();
-		for (int i = 0; i < messageList.size(); i++) {
-			indexes.add(i);
-		}
-
 		Collections.sort(alphabetList);
-		System.out.println(alphabetList);
 
+		// декодируем Б-У
 		for (int i = 0; i < messageList.size(); i++) {
+			// текущий символ верхней строки
 			String letter = alphabetList.get(index);
+
+			// записываем в конечную строку
 			decodedMessage.append(letter);
-			int j = 0;
-			for (Integer ind : indexes) {
-				if (messageList.get(ind).equals(letter)) {
-					j = ind;
-					indexes.remove(ind);
-					break;
+
+			// количество повторений символа в верхней строке перед текущим элементом
+			repeat = 0;
+
+			// позиция предыдущего символа в верхней строке
+			position = index - 1;
+
+			// считаем количество повторений символа в верхней строке среди предыдущих
+			while ((position >= 0) && (alphabetList.get(position).equals(letter))) {
+				repeat++;
+				position--;
+			}
+
+			// количество повторений символа в нижней строке
+			int sum = 0;
+
+			// проходимся по нижней строке
+			for (int j = 0; j < alphabetList.size(); j++) {
+				// находим соответствие между элементом верхней строки и элементом нижней
+				if (messageList.get(j).equals(letter)) {
+					// проверяем, какое по счету это повторение элемента
+					if (sum == repeat) {
+						// изменяем индекс текущего элемента, выходим из цикла если нашли нужное соответствие
+						index = j;
+						break;
+					}
+					// считаем повторения
+					sum++;
 				}
 			}
-			index = j;
 		}
 
 		return decodedMessage.toString();
